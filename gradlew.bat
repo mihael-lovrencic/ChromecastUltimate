3ab6fcbd16da2cec523139510e85860362823ba4
User <user@example.com>
2026-03-17 19:32:05 +0100

    Fix gradlew.bat exit code and disable config-cache for version tasks

--- gradlew.bat
+++ gradlew.bat
@@ -69,10 +69,11 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+set EXIT_CODE=%ERRORLEVEL%
 
 :end
 @rem End local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" endlocal
+if "%OS%"=="Windows_NT" endlocal & exit /b %EXIT_CODE%
 
 :fail
 exit /b 1
